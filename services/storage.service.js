380fa5ed2a34317290b9eb0380a85512d59e062f
Alexander Moiseyev <daklaktakpak@yandex.ru>
2022-07-21 22:30:57 +0300

    add api service

--- services/storage.service.js
+++ services/storage.service.js
@@ -3,6 +3,10 @@ import {join} from 'path';
 import {promises} from 'fs';
 
 const filePath = join(homedir(), 'weather-data.json');
+const TOKEN_DICTIONARY = {
+  token: 'token',
+  city: 'city'
+}
 
 const isExist = async (path) => {
   try {
@@ -32,4 +36,4 @@ const saveKeyValue = async (key, value) => {
   await promises.writeFile(filePath, JSON.stringify(data));
 };
 
-export {saveKeyValue, getKeyValue};
+export {saveKeyValue, getKeyValue, TOKEN_DICTIONARY};
