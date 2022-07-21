be461e5b719480fafadb25de7cfd67068f5198a9
Alexander Moiseyev <daklaktakpak@yandex.ru>
2022-07-21 23:07:30 +0300

    add error handler

--- weather.js
+++ weather.js
@@ -5,7 +5,7 @@ import {saveKeyValue, TOKEN_DICTIONARY} from './services/storage.service.js'
 import {getWeather} from "./services/api.service.js";
 
 const saveToken = async (token) => {
-  if(!token.length) {
+  if (!token.length) {
     printError('Не передан токен');
     return;
   }
@@ -15,20 +15,35 @@ const saveToken = async (token) => {
   } catch (e) {
     printError(e.message);
   }
-
 };
+
+const getForcast = async () => {
+  try {
+    // const weather = await getWeather('moscow')
+    const weather = await getWeather(process.env.CITY)
+    console.log(weather)
+  } catch (e) {
+    if (e?.response?.status === 404) {
+      printError('Неверно указан город')
+    } else if (e?.response?.status === 401) {
+      printError('Неверно указан токен')
+    } else {
+      printError(e.message)
+    }
+  }
+}
 
 const initCLI = () => {
   const args = getArgs(process.argv);
-  if(args.h) {
+  if (args.h) {
     printHelp();
   }
-  if(args.s) {
+  if (args.s) {
   }
-  if(args.t) {
+  if (args.t) {
     return saveToken(args.t);
   }
-  getWeather('moscow')
+  getForcast()
 };
 
 initCLI();
